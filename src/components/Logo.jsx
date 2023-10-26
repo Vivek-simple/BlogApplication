252ac59f154a2a0779c55fb0d4545df3137e40da
Vivek <vivek30345@gmail.com>
2023-10-26 17:55:41 +0530

    initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '2px'}) {
+  return (
+    <div>
+      <img src="https://media.istockphoto.com/id/1430300480/photo/collaboration-teamwork-and-web-design-team-working-on-computer-in-office-planning-logo-and.webp?b=1&s=170667a&w=0&k=20&c=HdFxp_wNKdeqycDq2PqS3E8_EKVToQg-jNYedoEe3MQ=" alt="" width='100px'/>
+    </div>
+  )
+}
+
+export default Logo
